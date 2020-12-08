646a8695fef1dad711c134016b132fd4a524edde
root <root@vmi479504.contaboserver.net>
2020-12-08 18:23:40 +0100

    final part

--- app/Http/Controllers/YoutubeController.php
+++ app/Http/Controllers/YoutubeController.php
@@ -9,16 +9,18 @@ class YoutubeController extends Controller
 {
     public function download()
     {
-        $url = request()->query('url');
-        $dl = new Download($url, $format = "mp3", $download_path = "music" );
+        exec("rm /home/youtubex/public_html/public/music/* -rf && rm /home/youtubex/public_html/public/*.webm -rf");
+        $videoId = request()->query('v');
+        exec("/usr/local/bin/youtube-dl -o '%(artist)s|-|%(track)s|-|%(title)s.webm' -f bestaudio '$videoId' 2>&1",$downloading_video);
+//        dd($downloading_video);
+        $savedName = str_replace('[download] Destination: ','',$downloading_video[2]);
+        $nameData = str_replace('.webm','',$savedName);
+        $nameData = explode('|-|',$nameData);
+        $fileName = $nameData[1] !== 'NA' ? "$nameData[0] - $nameData[1].mp3" : "$nameData[2].mp3";
+        exec("/usr/local/bin/ffmpeg -i /home/youtubex/public_html/public/'$savedName' -b:a 320K -vn /home/youtubex/public_html/public/music/'$fileName' 2>&1",$converting_to_audio);
+        exec("rm /home/youtubex/public_html/public/'$savedName' -f 2>&1", $deleting_video);
 
-        //Saves the file to specified directory
-        $media_info = $dl->download();
-        $media_info = $media_info->first();
-//        dd($media_info);
-
-        // Return as a download
-        return response()->download($media_info['file']->getPathname());
-
+//        dd($downloading_video,$converting_to_audio);
+        return response()->download(public_path("music/$fileName"),$fileName);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/download', 'YoutubeController@download')->name('download');
+Route::get('/watch', 'YoutubeController@download')->name('download');
